b6b4e582afdaafcdd04a216963bc8bb151cae9fd
Aleksa <aleksa1994@hotmail.com>
2016-08-13 20:24:06 +0200

    Implement base layout with dummy data

--- app/src/main/java/rs/htec/aleksa/htectest/MainActivity.java
+++ app/src/main/java/rs/htec/aleksa/htectest/MainActivity.java
@@ -1,13 +1,29 @@
 package rs.htec.aleksa.htectest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_list_view)
+    ListView mListView;
+
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mAdapter = new ArrayAdapter<String>(
+                this, R.layout.main_list_view_item, R.id.main_item_description, DummyData.DUMMY_DATA);
+
+        mListView.setAdapter(mAdapter);
     }
 }
